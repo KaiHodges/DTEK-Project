36085f0f9cbdbe76eeafbd9b832b43921afe4c95
khodges <khodges@kth.se>
2025-11-23 15:30:14 +0100

    minor fixes and additions

--- Display/vga.c
+++ Display/vga.c
@@ -1,28 +1,58 @@
-#define VGA_PIXEL_BASE 0x08000000; //framebuffer base, memory address for the framebuffer 
-#define VGA_DMA_BASE 0x04000100; //address for DMA ctrl interactions
+#include <stdio.h>
+#include <stdlib.h>
+#include "vga.h"
 
+#define CELL_SIZE   12
+#define GRID_COLS   10
+#define GRID_ROWS   20
+#define GRID_LEFT   100
+#define GRID_TOP    20
 
-static volatile unsigned char* const VGA_PIXEL = (unsigned char*) VGA_PIXEL_BASE;
-static volatile unsigned int* const VGA_CTRL = (unsigned char*) VGA_DMA_BASE;
+volatile unsigned char *frame0 = (unsigned char*) 0x08000000;
+volatile unsigned char *frame1 = (unsigned char*) 0x08000000 + SCREEN_WIDTH * SCREEN_HEIGHT;
+volatile unsigned int  *vga_ctrl = (unsigned int*) 0x04000100;
 
-
-void place_pixel(int x, int y, unsigned char color) 
+void put_pixel(int x, int y, unsigned char color) 
 {
-
-	if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) 
-		return; 
+    if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) {
+        return;
+    } 
 		
-	VGA_PIXEL[y * SCREEN_WIDTH + x] = color; 
-
+    frame0[y * SCREEN_WIDTH + x] = color; 
 } 
 
 void clear_screen(unsigned char color)
 {
- for(int y = 0; y < SCREEN_HEIGHT; y++) {
-		for (int x = 0; x < SCREEN_WIDTH; x++) {
-			VGA_PIXEL[y * SCREEN_WIDTH + x] = color;
-		}
-
-	}
-
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        frame0[i] = color; 
+    }
 }
+
+// Draw a 12x12 pixel square outline
+void draw_square(int x, int y, unsigned char color) 
+{	
+    int x_end = x + CELL_SIZE - 1; 
+    int y_end = y + CELL_SIZE - 1; 
+
+    for (int xx = x; xx <= x_end; xx++) {
+        put_pixel(xx, y,     color);
+        put_pixel(xx, y_end, color); 
+    }
+		
+    for (int yy = y; yy <= y_end; yy++) {
+        put_pixel(x,     yy, color);
+        put_pixel(x_end, yy, color);
+    }
+}
+
+void draw_grid(void) 
+{	
+    for (int col = 0; col < GRID_COLS; col++) {
+        for (int row = 0; row < GRID_ROWS; row++) {
+            int x = GRID_LEFT + col * CELL_SIZE;
+            int y = GRID_TOP  + row * CELL_SIZE;
+            draw_square(x, y, COLOR_WHITE);
+        }
+    }
+}
+
--- Display/vga.h
+++ Display/vga.h
@@ -2,5 +2,19 @@
 #define SCREEN_HEIGHT 240;
 
 
+// Convert 3:3:2 RGB to one byte (RGB332)
+#define RGB332(r,g,b) ( ((r & 0x7) << 5) | ((g & 0x7) << 2) | ((b & 0x3)) )
+
+// Basic colors
+#define COLOR_BLACK       RGB332(0, 0, 0)
+#define COLOR_WHITE       RGB332(7, 7, 3)
+
+
+void place_pixel(int x, int y, unsigned char color);
+void clear_screen(unsigned char color);
+
+
+
+
 
 
